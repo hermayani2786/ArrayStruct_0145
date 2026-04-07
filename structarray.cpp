52d153d9636ea02d93b4d486290f44ba199cab18
hermayani2786 <herma.yani2786@gmail.com>
2026-04-07 19:09:44 +0700

    membuat struct detaiAlamat

--- structarray.cpp
+++ structarray.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
 using namespace std;
 
-struct Orang
+struct detailAlamat
 {
-    string nama;
-    string alamat;
-    int umur;
+    string desa;
+    string kota;
 };
-int main()
-{
-    //Deklarasi variabel struct
-    Orang mhs;
-    //Mengisi data
-    cout << "Isikan data nama : ";
-    cin >> mhs.nama;
-    cout << "Isikan data alamat : ";
-    cin >> mhs.alamat;
-    cout << "Isikan data umur : ";
-    cin >> mhs.umur;
-    cout << endl;
-
-    cout << "Data Tampil" << endl;
-    cout << "Nama : " << mhs.nama << endl;
-    cout << "Alamat : " << mhs.alamat << endl;
-    cout << "Umur : " << mhs.umur << endl;
-
-
-}
